8dbc27d353eeafd118b7f2306066350dc3112e02
Sai Krishna M <m.s.krishna310@gmail.com>
2024-06-03 16:06:17 +0530

    Add files via upload

---
+++ graph.js
@@ -0,0 +1,442 @@
+let data = '{"pageMetricList":[{"name":"Followers","val":"204"},{"name":"Impressions","val":"6","pill":{"val":"50%","type":"INCREASE","desc":"in last 7 days"}},{"name":"Profile Views","val":"6","pill":{"val":"50%","type":"INCREASE","desc":"in last 7 days"}},{"name":"Reach","val":"2","pill":{"val":"100%","type":"INCREASE","desc":"in last 7 days"}},{"name":"Website Clicks","val":"0","pill":{"val":"0%","type":"INCREASE","desc":"in last 7 days"}}],"graphList":[{"graph":{"name":"New Followers","type":"line","desc":"Discover the number of New Followers that you have gained since last period.","headers":{"v1":"New Followers","v2":"Lost Followers"},"colors":{"v1":"#1E5CFF","v2":"#AC71F0"},"data":[{"label":"2024-05-22","v1":"0","v2":"0"},{"label":"2024-05-23","v1":"0","v2":"0"},{"label":"2024-05-24","v1":"0","v2":"0"},{"label":"2024-05-25","v1":"1","v2":"0"},{"label":"2024-05-26","v1":"0","v2":"0"},{"label":"2024-05-27","v1":"0","v2":"0"}]},"insights":{"header":{"text":"Followers Insights","desc":"Since Previous Period"},"pills":[{"val":"100%","type":"INCREASE","desc":"New Followers"},{"val":"0%","type":"INCREASE","desc":"Lost Followers"}],"content":{"type":"LINKED_TABLE","data":[1,0]}}},{"graph":{"name":"Audience Reach Demographic","type":"bar","desc":"Understand your audience demographics based on age and gender","headers":{"v1":"Male","v2":"Female","v3":"Unspecified"},"colors":{"v1":"#BC9AFF","v2":"#9FEEFF","v3":"#8EADFF"},"data":[{"label":"13-17","v1":"7","v3":"2"},{"label":"18-24","v1":"60","v2":"9","v3":"7"},{"label":"25-34","v1":"78","v2":"4","v3":"2"},{"label":"35-44","v1":"5","v2":"2","v3":"2"},{"label":"45-54","v1":"1"},{"label":"65+","v3":"1"}]},"insights":{"header":{"text":"Gender Insights","desc":"Since Previous Period"},"content":{"type":"DOUGHNUT","data":[{"label":"Male","val":"83","color":"#BC9AFF"},{"label":"Female","val":"8","color":"#9FEEFF"},{"label":"Unspecified","val":"7","color":"#8EADFF"}]}}}],"locGraphs":{"Cities":{"name":"Locations","type":"horizontal_bar","desc":"See the places where your followers are concentrated.","headers":{"v1":"viewer"},"colors":{"v1":"#8EADFF"},"data":[{"label":"Bangalore, Karnataka","v1":"21"},{"label":"Gaya, Bihar","v1":"14"},{"label":"Kolkata, West Bengal","v1":"7"},{"label":"Gurua, Bihar","v1":"7"},{"label":"Delhi, Delhi","v1":"6"},{"label":"Bodh Gaya, Bihar","v1":"6"},{"label":"Mumbai, Maharashtra","v1":"5"},{"label":"Gurugram, Haryana","v1":"4"},{"label":"Buddha Gaya, Bihar","v1":"3"},{"label":"Deoghar, Jharkhand","v1":"3"},{"label":"Patna, Bihar","v1":"3"},{"label":"Hisua, Bihar","v1":"3"},{"label":"Hyderabad, Telangana","v1":"3"},{"label":"Dehri, Bihar","v1":"2"},{"label":"Darbhanga, Bihar","v1":"2"},{"label":"Bilari, Uttar Pradesh","v1":"2"},{"label":"Pakaur, Jharkhand","v1":"2"},{"label":"Sanand, Gujarat","v1":"2"},{"label":"Manama, Capital Governorate","v1":"2"},{"label":"Faridabad, Haryana","v1":"2"},{"label":"Dubai, Dubai","v1":"2"},{"label":"Mahasamund, Chhattisgarh","v1":"2"},{"label":"Noida, Uttar Pradesh","v1":"2"},{"label":"Banei, Odisha","v1":"2"},{"label":"Thane, Maharashtra","v1":"1"},{"label":"Dumka, Jharkhand","v1":"1"},{"label":"Jamtara, Jharkhand","v1":"1"},{"label":"Chunar, Uttar Pradesh","v1":"1"},{"label":"Yamunanagar, Haryana","v1":"1"},{"label":"Biswanath Chariali, Assam","v1":"1"},{"label":"Khobar, Eastern Province","v1":"1"},{"label":"Arwal, Bihar","v1":"1"},{"label":"Naugachhia, Bihar","v1":"1"},{"label":"Dudhichua, Madhya Pradesh","v1":"1"},{"label":"Champua, Odisha","v1":"1"},{"label":"Pleiku, Gia Lai Province","v1":"1"},{"label":"Jakarta, Jakarta","v1":"1"},{"label":"Jodhpur, Rajasthan","v1":"1"},{"label":"Gauhati, Assam","v1":"1"},{"label":"Kurukshetra, Haryana","v1":"1"},{"label":"Greater Noida, Uttar Pradesh","v1":"1"},{"label":"Bhokar, Maharashtra","v1":"1"},{"label":"Srinagar, Jammu and Kashmir","v1":"1"},{"label":"Sherghati, Bihar","v1":"1"},{"label":"Chittaranjan, Jharkhand","v1":"1"}]},"Countries":{"name":"Locations","type":"horizontal_bar","desc":"See the places where your followers are concentrated.","headers":{"v1":"viewer"},"colors":{"v1":"#8EADFF"},"data":[{"label":"India","v1":"163"},{"label":"United Arab Emirates","v1":"4"},{"label":"Saudi Arabia","v1":"4"},{"label":"Bahrain","v1":"2"},{"label":"Hong Kong SAR China","v1":"1"},{"label":"Australia","v1":"1"},{"label":"Vietnam","v1":"1"},{"label":"Indonesia","v1":"1"},{"label":"Turkey","v1":"1"},{"label":"United States","v1":"1"},{"label":"Sri Lanka","v1":"1"}]}},"instaHeatMap":{"name":"Followers Online Activity","desc":"Glance at your followers activity to determine when your audience is most likely to engage with your content.","xHeaders":["00:00 hrs","02:00 hrs","04:00 hrs","06:00 hrs","08:00 hrs","10:00 hrs","12:00 hrs","14:00 hrs","16:00 hrs","18:00 hrs","20:00 hrs","22:00 hrs"],"yHeaders":["Sun","Sat","Fri","Thr","Wed","Tue","Mon"],"data":[{"x":0,"y":0,"v":0},{"x":1,"y":0,"v":0},{"x":2,"y":0,"v":0},{"x":3,"y":0,"v":0},{"x":4,"y":0,"v":0},{"x":5,"y":0,"v":0},{"x":6,"y":0,"v":0},{"x":7,"y":0,"v":0},{"x":8,"y":0,"v":0},{"x":9,"y":0,"v":0},{"x":10,"y":0,"v":0},{"x":11,"y":0,"v":0},{"x":0,"y":1,"v":99},{"x":1,"y":1,"v":82},{"x":2,"y":1,"v":85},{"x":3,"y":1,"v":92},{"x":4,"y":1,"v":95},{"x":5,"y":1,"v":91},{"x":6,"y":1,"v":35},{"x":7,"y":1,"v":16},{"x":8,"y":1,"v":22},{"x":9,"y":1,"v":49},{"x":10,"y":1,"v":87},{"x":11,"y":1,"v":72},{"x":0,"y":2,"v":103},{"x":1,"y":2,"v":82},{"x":2,"y":2,"v":100},{"x":3,"y":2,"v":104},{"x":4,"y":2,"v":103},{"x":5,"y":2,"v":78},{"x":6,"y":2,"v":53},{"x":7,"y":2,"v":22},{"x":8,"y":2,"v":19},{"x":9,"y":2,"v":41},{"x":10,"y":2,"v":83},{"x":11,"y":2,"v":93},{"x":0,"y":3,"v":93},{"x":1,"y":3,"v":97},{"x":2,"y":3,"v":85},{"x":3,"y":3,"v":96},{"x":4,"y":3,"v":95},{"x":5,"y":3,"v":83},{"x":6,"y":3,"v":48},{"x":7,"y":3,"v":14},{"x":8,"y":3,"v":22},{"x":9,"y":3,"v":49},{"x":10,"y":3,"v":62},{"x":11,"y":3,"v":95},{"x":0,"y":4,"v":87},{"x":1,"y":4,"v":78},{"x":2,"y":4,"v":93},{"x":3,"y":4,"v":91},{"x":4,"y":4,"v":114},{"x":5,"y":4,"v":84},{"x":6,"y":4,"v":38},{"x":7,"y":4,"v":18},{"x":8,"y":4,"v":23},{"x":9,"y":4,"v":53},{"x":10,"y":4,"v":84},{"x":11,"y":4,"v":100},{"x":0,"y":5,"v":0},{"x":1,"y":5,"v":0},{"x":2,"y":5,"v":0},{"x":3,"y":5,"v":0},{"x":4,"y":5,"v":0},{"x":5,"y":5,"v":0},{"x":6,"y":5,"v":0},{"x":7,"y":5,"v":0},{"x":8,"y":5,"v":0},{"x":9,"y":5,"v":0},{"x":10,"y":5,"v":0},{"x":11,"y":5,"v":0},{"x":0,"y":6,"v":0},{"x":1,"y":6,"v":0},{"x":2,"y":6,"v":0},{"x":3,"y":6,"v":0},{"x":4,"y":6,"v":0},{"x":5,"y":6,"v":0},{"x":6,"y":6,"v":0},{"x":7,"y":6,"v":0},{"x":8,"y":6,"v":0},{"x":9,"y":6,"v":0},{"x":10,"y":6,"v":0},{"x":11,"y":6,"v":0}]},"channelId":24}';
+let insightsData = JSON.parse(data);
+// alert(insightsData);
+// console.log(insightsData);
+
+function renderGraphs(){
+    insightsData.graphList.forEach(function(graphItem) {
+        createChart(graphItem.graph);
+
+        if('insights' in graphItem && graphItem.insights !== null){
+            createChartForInsights(graphItem.insights)
+        }
+    });
+
+    if('locGraphs' in insightsData){
+        createChart(insightsData.locGraphs.Cities)
+        createChart(insightsData.locGraphs.Countries)
+    }
+
+    if('instaHeatMap' in insightsData){
+        getHeatMap(insightsData.instaHeatMap)
+    }
+}
+
+
+function createChart(graph) {
+    var containerId = 'chart-' + Math.floor(Math.random() * 1000000);
+    var container = $('<div id="' + containerId + '" style="width: 100%; height: 800px;"></div>');
+    $('#chart-container').append(container);
+
+    switch(graph.type) {
+        case 'line':
+            getMultiLineGraph(containerId, graph);
+            break;
+        case 'bar':
+            getMultiVerticalBarGraph(containerId, graph);
+            break;
+        case 'doughnut':
+            createPieChart(containerId, graph);
+            break;
+        // Add more cases as needed for different types of charts
+        case 'horizontal_bar':
+            getHorizontalBarGraph(containerId,graph);
+            break;
+        default:
+            console.log("This kind of graph not found.");
+
+    }
+}
+
+
+function  createChartForInsights( insights){
+    var containerId = 'insight-' + Math.floor(Math.random() * 1000000);
+    var container = $('<div id="' + containerId + '" style="width: 100%; height: 800px;"></div>');
+
+    console.log("Insights",insights);
+    switch (insights.content.type){
+        case 'TABLE' :
+            // $('#chart-container').append(container);
+            break;
+        case 'LINKED_TABLE':
+            // $('#chart-container').append(container);
+            break;
+        case  'DEMOGRAPHY':
+            // $('#chart-container').append(container);
+            break;
+        case 'DOUGHNUT' :
+            $('#chart-container').append(container);
+            getPieGraph(containerId,insights.content.data,insights.header);
+            break;
+        case 'ENGAGEMENT_INSIGHTS':
+            // $('#chart-container').append(container);
+            break;
+        case  'AUDIENCE_LOCATION':
+            // $('#chart-container').append(container);
+            break;
+        default:
+            console.log("This kind of insights not found.")
+    }
+}
+
+function  getPieGraph(containerId,data,header) {
+    // create data
+    // var data = [
+    //     {x: "A", value: 637166},
+    //     {x: "B", value: 721630},
+    //     {x: "C", value: 148662},
+    //     {x: "D", value: 78662},
+    //     {x: "E", value: 90000}
+    // ];
+
+
+
+    var dataList = data.map(item => {
+        return {x:item.label,value: item.val,normal: {fill : item.color}};
+    });
+
+
+    console.log("DataList ",dataList);
+    var  chart = anychart.pie(dataList)
+
+
+    chart.title(header.text);
+
+
+    // configure outlines
+    // chart.normal().outline().enabled(true);
+    // chart.normal().outline().width("5%");
+    // chart.hovered().outline().width("10%");
+    // chart.selected().outline().width("3");
+    // chart.selected().outline().fill("#455a64");
+    // chart.selected().outline().stroke(null);
+    // chart.selected().outline().offset(2);
+
+    // set the chart title
+    // chart.title(title);
+
+    chart.container(containerId);
+    chart.draw();
+}
+
+function getMultiVerticalBarGraph(containerId,graph){
+
+    var dataList = graph.data.map(item => {
+        var result = [item.label];
+        Object.keys(item).forEach(key => {
+            if (key.startsWith('v')) {
+                result.push(parseFloat(item[key]));
+            }
+        });
+        return result;
+    });
+    var dataSet =  anychart.data.set(dataList);
+
+    var chart = anychart.column();
+
+
+    var index = 1;
+    Object.keys(graph.headers).forEach(key => {
+        if (graph.headers.hasOwnProperty(key)) {
+            console.log(key)
+            var seriesData = dataSet.mapAs({ x: 0, value: index });
+            var series = chart.column(seriesData);
+            series.name(graph.headers[key]);
+            series.color(graph.colors[key]);
+            index++;
+        }
+    });
+
+
+
+    chart.animation(true);
+
+    chart.yAxis().labels().format('{%Value}{groupsSeparator: }');
+
+    chart.yAxis().title('Revenue');
+
+    chart
+        .labels()
+        .enabled(true)
+        .position('center-top')
+        .anchor('center-bottom')
+        .format('{%Value}{groupsSeparator: }');
+    chart.hovered().labels(false);
+
+// turn on legend and tune it
+    chart.legend().enabled(true).fontSize(13).padding([0, 0, 20, 0]);
+
+// interactivity settings and tooltip position
+    chart.interactivity().hoverMode('single');
+
+    chart
+        .tooltip()
+        .positionMode('point')
+        .position('center-top')
+        .anchor('center-bottom')
+        .offsetX(0)
+        .offsetY(5)
+        .titleFormat('{%X}')
+        .format('{%SeriesName} : {%Value}{groupsSeparator: }');
+
+
+    chart.yGrid(true);
+// set grid stroke appearance
+    chart.yGrid().stroke({color: "#CECECE"});
+
+// enable grid
+    chart.xGrid(true);
+// set grid stroke appearance
+    chart.xGrid().stroke({color: "#CECECE"});
+
+
+// Set chart container and draw the chart
+    chart.container(containerId);
+    chart.draw();
+}
+
+function  getMultiLineGraph(containerId,graph){
+
+    var chart = anychart.line();
+
+    var dataSet = anychart.data.set(graph.data.map(item => {
+        var result = [item.label];
+        Object.keys(item).forEach(key => {
+            if (key.startsWith('v')) {
+                result.push(parseFloat(item[key]));
+            }
+        });
+        return result;
+    }));
+
+    var index = 1;
+    Object.keys(graph.headers).forEach(key => {
+        if (graph.headers.hasOwnProperty(key)) {
+            var seriesData = dataSet.mapAs({ x: 0, value: index });
+            var series = chart.line(seriesData);
+            series.name(graph.headers[key]);
+            series.color(graph.colors[key]);
+            series.markers(true);
+            series.markers().type("circle");
+            series.markers().size(5);
+            series.markers().zIndex(100);
+            index++;
+        }
+    });
+
+
+    chart.title(graph.name);
+
+// // set chart data
+//         chart.data();
+//
+//         var dataSet = anychart.data.set(data);
+//
+// // map data for the first series, take x from the zero column and value from the first column of data set
+//         var firstSeriesData = dataSet.mapAs({ x: 0, value: 1 });
+//
+// // map data for the second series, take x from the zero column and value from the second column of data set
+//         var secondSeriesData = dataSet.mapAs({ x: 0, value: 2 });
+//
+//         var thirdSeriesData = dataSet.mapAs({ x: 0, value: 3 })
+
+// adding dollar symbols to yAxis labels
+    chart.yAxis().labels().format('{%Value}');
+
+
+
+// turn on chart animation
+    chart.animation(true);
+
+// turn on the crosshair
+    chart
+        .labels()
+        .enabled(true)
+        .position('center-top')
+        .anchor('center-bottom')
+        .format('{%Value}{groupsSeparator: }');
+    chart.hovered().labels(false);
+
+    chart
+        .yAxis()
+        .labels({ padding: [5, 5, 0, 5] });
+
+// set chart title text settings
+//         chart.title(
+//             'The cost of ACME\'s shares\ncompared with their main competitor during month'
+//         );
+
+
+
+// // create first series with mapped data
+//         var firstSeries = chart.spline(firstSeriesData);
+//         firstSeries.name('Male');
+//         firstSeries.markers(true);
+//         firstSeries.markers().type("circle");
+//         firstSeries.markers().size(5);
+//         firstSeries.markers().zIndex(100);
+//
+// // create second series with mapped data
+//         var secondSeries = chart.spline(secondSeriesData);
+//         secondSeries.name('Female');
+//         secondSeries.markers(true);
+//         secondSeries.markers().type("circle");
+//         secondSeries.markers().size(5);
+//         secondSeries.markers().zIndex(100);
+//
+//         var thirdSeries = chart.spline(thirdSeriesData);
+//         thirdSeries.name('Unspecified');
+//         thirdSeries.markers(true);
+//         thirdSeries.markers().type("circle");
+//         thirdSeries.markers().size(5);
+//         thirdSeries.markers().zIndex(100);
+
+
+// turn the legend on
+    chart.legend().enabled(true).fontSize(13).padding([0, 0, 20, 0]);
+
+
+    chart.yGrid(true);
+// set grid stroke appearance
+    chart.yGrid().stroke({color: "#CECECE"});
+
+// enable grid
+    chart.xGrid(true);
+// set grid stroke appearance
+    chart.xGrid().stroke({color: "#CECECE"});
+
+
+// Set chart container and draw the chart
+    chart.container(containerId);
+    chart.draw();
+
+}
+
+function getHorizontalBarGraph(containerId,graph){
+
+    var dataList = graph.data.map(item => {
+        var result = [item.label];
+        Object.keys(item).forEach(key => {
+            if (key.startsWith('v')) {
+                result.push(parseFloat(item[key]));
+            }
+        });
+        return result;
+    });
+    var dataSet =  anychart.data.set(dataList);
+
+    var chart = anychart.bar();
+
+
+    var index = 1;
+    Object.keys(graph.headers).forEach(key => {
+        if (graph.headers.hasOwnProperty(key)) {
+            console.log(key)
+            var seriesData = dataSet.mapAs({ x: 0, value: index });
+            var series = chart.bar(seriesData);
+            series.name(graph.headers[key]);
+            series.color(graph.colors[key]);
+            index++;
+        }
+    });
+// turn on chart animation
+    chart.animation(true);
+
+    chart.xAxis().labels().format('{%Value}{groupsSeparator: }');
+
+// set titles for Y-axis
+//         chart.yAxis().title('Revenue');
+
+    chart
+        .labels()
+        .enabled(true)
+        .position('right-center')
+        .anchor('left-center')
+        .format('{%Value}{groupsSeparator: }');
+    chart.hovered().labels(false);
+
+// // turn on legend and tune it
+//         chart.legend().enabled(true).fontSize(13).padding([0, 0, 20, 0]);
+
+// interactivity settings and tooltip position
+    chart.interactivity().hoverMode('single');
+
+    chart
+        .tooltip()
+        .positionMode('point')
+        .position('center-top')
+        .anchor('center-bottom')
+        .offsetX(0)
+        .offsetY(5)
+        .titleFormat('{%X}')
+        .format('{%SeriesName} : {%Value}{groupsSeparator: }');
+
+    chart.yGrid(true);
+// set grid stroke appearance
+    chart.yGrid().stroke({color: "#CECECE"});
+
+// enable grid
+    chart.xGrid(true);
+// set grid stroke appearance
+    chart.xGrid().stroke({color: "#CECECE"});
+
+
+// Set chart container and draw the chart
+    chart.container(containerId);
+    chart.draw();
+}
+
+function  getHeatMap(heatData){
+
+    // var data = [
+    //     {x: "2010", y: "A", heat: 15},
+    //     {x: "2011", y: "A", heat: 17},
+    //     {x: "2012", y: "A", heat: 21},
+    //     {x: "2013", y: "A", heat: 23},
+    //     {x: "2010", y: "B", heat: 34},
+    //     {x: "2011", y: "B", heat: 33},
+    //     {x: "2012", y: "B", heat: 32},
+    //     {x: "2013", y: "B", heat: 30},
+    //     {x: "2010", y: "C", heat: 43},
+    //     {x: "2011", y: "C", heat: 42},
+    //     {x: "2012", y: "C", heat: 40},
+    //     {x: "2013", y: "C", heat: 38},
+    //     {x: "2010", y: "D", heat: 8},
+    //     {x: "2011", y: "D", heat: 8},
+    //     {x: "2012", y: "D", heat: 7},
+    //     {x: "2013", y: "D", heat: 8}
+    // ];
+    // create a chart and set the data
+
+    var containerId = 'chart-' + Math.floor(Math.random() * 1000000);
+    var container = $('<div id="' + containerId + '" style="width: 100%; height: 800px;"></div>');
+    $('#chart-container').append(container);
+    //
+    var data = [];
+
+    heatData.data.forEach(function(item) {
+        console.log(item)
+        var x = heatData.xHeaders[item.x];
+        var y = heatData.yHeaders[item.y];
+        var heat = item.v;
+
+        data.push({ x: x, y: y, heat: heat });
+    });
+
+    // console.log("Heat Map Data", data)
+
+    var chart = anychart.heatMap(data);
+
+    var labels = chart.xAxis();
+
+    labels.offsetX
+
+    // set the chart title
+    chart.title(heatData.name);
+
+    // set the container id
+    chart.container(containerId);
+
+    // initiate drawing the chart
+    chart.draw();
+}
